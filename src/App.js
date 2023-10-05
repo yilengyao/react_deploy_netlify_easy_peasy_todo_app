f07a8d8bd579b6c1a1ff1e75ef8690f6b1df9518
YilengYao <yilengyao@gmail.com>
2023-10-05 07:18:25 -0700

    migrate nav to easy peasy

--- src/App.js
+++ src/App.js
@@ -9,15 +9,31 @@ import About from "./About";
 import Missing from "./Missing";
 import { Routes, Route } from "react-router-dom";
 import { DataProvider } from "./context/DataContext";
+import { useEffect } from "react";
+import useAxiosFetch from "./hooks/useAxiosFetch";
+import { useStoreActions } from "easy-peasy";
 
 function App() {
+  const setPosts = useStoreActions((actions) => actions.setPosts);
+  const { data, fetchError, isLoading } = useAxiosFetch(
+    "http://localhost:3500/posts",
+  );
+
+  useEffect(() => {
+    setPosts(data);
+  }, [data, setPosts]);
+
   return (
     <div className="App">
       <Header title="React JS Blog" />
       <DataProvider>
         <Nav />
         <Routes>
-          <Route exact path="/" element={<Home />} />
+          <Route
+            exact
+            path="/"
+            element={<Home isLoading={isLoading} fetchError={fetchError} />}
+          />
           <Route exact path="/post" element={<NewPost />} />
           <Route path="/edit/:id" element={<EditPost />} />
           <Route path="/post/:id" element={<PostPage />} />
